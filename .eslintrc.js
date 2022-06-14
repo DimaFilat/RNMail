a768d13f5421804c2c9688765674b819510bc652
Dmitriy Filatchev <o6jlako@mail.ru>
2022-06-14 04:39:08 +0300

    intial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: 'prettier',
+  rules: {
+    '@typescritp-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-shadow': 1,
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+};
